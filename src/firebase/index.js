bbe37362766f23791b0ee73815d20f244389ee6f
GnauqTahnAl <gnauq.tahn.al@gmail.com>
2023-11-08 20:29:28 +0700

    add firebase and expo barcodescanner

---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAnnbUuMr2dLvEuke4-7LOpeS4qbhHzz0I',
+  authDomain: 'cbook-afa1a.firebaseapp.com',
+  projectId: 'cbook-afa1a',
+  storageBucket: 'cbook-afa1a.appspot.com',
+  messagingSenderId: '921899358959',
+  appId: '1:921899358959:web:9d42df74e3ff008069591a',
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
